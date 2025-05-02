009e1bf2d6a0d4f7ad484d74ea60f40a62ef76ec
Colin Walters <walters@verbum.org>
2025-05-02 10:56:01 -0400

    hack: Rework+cleanup container build
    Main motivation: I was looking at making more changes here
    
    - Use an idiom I'd like to standardize more of copy context to `FROM scratch` image
      which is then mounted and consumed in other phases by mounting. This helps
      avoid polluting later containers with intermediate copied files.
    - Change `build.sh` to handle being run from any directory
    - Drop the `dev-rootfs` stuff as it's weird and awkward; instead we should
      encourage multi-step builds deriving from this image
    - Don't make `bootc.tar.zst` only to immediately untar it; just use `COPY`
      from the build container
    - Use heredocs to condense multiple `RUN` invocations to avoid pointless
      small layers
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- hack/build.sh
+++ hack/build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -xeu
 . /usr/lib/os-release
+dn=$(cd $(dirname $0) && pwd)
 case $ID in
   centos|rhel) dnf config-manager --set-enabled crb;;
   fedora) dnf -y install dnf-utils 'dnf5-command(builddep)';;
 esac
-dnf -y builddep ./contrib/packaging/bootc.spec
+dnf -y builddep ${dn}/../contrib/packaging/bootc.spec
 # Extra dependencies
 dnf -y install git-core
