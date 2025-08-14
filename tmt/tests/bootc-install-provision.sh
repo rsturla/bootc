e7a3835946f4cef24fcde0c5460cbd1d5d6f9b59
Colin Walters <walters@verbum.org>
2025-08-14 17:32:22 +0200

    ci/tmt: distro-sync ostree+systemd
    This is a hackaround for https://gitlab.com/redhat/centos-stream/containers/bootc/-/issues/1174
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- tmt/tests/bootc-install-provision.sh
+++ tmt/tests/bootc-install-provision.sh
@@ -119,6 +119,8 @@ case $ID in
   centos|rhel) dnf config-manager --set-enabled crb;;
   fedora) dnf -y install dnf-utils 'dnf5-command(builddep)';;
 esac
+# Handle version skew
+dnf -y distro-sync ostree{,-libs} systemd
 dnf -y builddep contrib/packaging/bootc.spec
 # Extra dependencies
 dnf -y install git-core
