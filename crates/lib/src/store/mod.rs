82057f869c53ebd3b53cd0aeb002e5e6e5caadbf
Colin Walters <walters@verbum.org>
2025-08-15 09:28:40 +0200

    store: Remove dynamic abstraction
    A while ago we added a trait in preparation for multiple backends.
    The current composefs branch ignores it and has a bunch of
    `if {} else {}`.
    
    Looking at this, what I think will work better in the end is
    actually a more refined version of the `if {} else {}` model
    instead of trying to really flesh out this trait. It's
    hard to say of course until we get there, but the trait
    approach forces a high level of abstraction vs just trying
    to factor out common code between two backends.
    
    Signed-off-by: Colin Walters <walters@verbum.org>

--- crates/lib/src/store/mod.rs
+++ crates/lib/src/store/mod.rs
@@ -1,5 +1,4 @@
 use std::cell::OnceCell;
-use std::env;
 use std::ops::Deref;
 use std::sync::Arc;
 
@@ -7,20 +6,15 @@ use anyhow::{Context, Result};
 use cap_std_ext::cap_std;
 use cap_std_ext::cap_std::fs::{Dir, DirBuilder, DirBuilderExt as _};
 use cap_std_ext::dirext::CapStdExtDirExt;
-use clap::ValueEnum;
 use fn_error_context::context;
 
-use ostree_ext::container::OstreeImageReference;
-use ostree_ext::keyfileext::KeyFileExt;
+use composefs;
 use ostree_ext::sysroot::SysrootLock;
-use ostree_ext::{composefs, ostree};
 use rustix::fs::Mode;
 
 use crate::lsm;
 use crate::spec::ImageStatus;
 use crate::utils::deployment_fd;
-
-mod ostree_container;
 
 /// See https://github.com/containers/composefs-rs/issues/159
 pub type ComposefsRepository =
@@ -50,31 +44,12 @@ pub(crate) struct Storage {
 
     /// Our runtime state
     run: Dir,
-
-    /// This is a stub abstraction that tries to hide ostree
-    /// that we aren't really using right now
-    pub store: Box<dyn ContainerImageStoreImpl>,
 }
 
 #[derive(Default)]
 pub(crate) struct CachedImageStatus {
     pub image: Option<ImageStatus>,
     pub cached_update: Option<ImageStatus>,
-}
-
-pub(crate) trait ContainerImageStore {
-    fn store(&self) -> Result<Option<Box<dyn ContainerImageStoreImpl>>>;
-}
-
-pub(crate) trait ContainerImageStoreImpl {
-    fn spec(&self) -> crate::spec::Store;
-
-    fn imagestatus(
-        &self,
-        sysroot: &SysrootLock,
-        deployment: &ostree::Deployment,
-        image: OstreeImageReference,
-    ) -> Result<CachedImageStatus>;
 }
 
 impl Deref for Storage {
@@ -88,15 +63,6 @@ impl Deref for Storage {
 impl Storage {
     pub fn new(sysroot: SysrootLock, run: &Dir) -> Result<Self> {
         let run = run.try_clone()?;
-        let store = match env::var("BOOTC_STORAGE") {
-            Ok(val) => crate::spec::Store::from_str(&val, true).unwrap_or_else(|_| {
-                let default = crate::spec::Store::default();
-                tracing::warn!("Unknown BOOTC_STORAGE option {val}, falling back to {default:?}");
-                default
-            }),
-            Err(_) => crate::spec::Store::default(),
-        };
-        let store = load(store);
 
         // ostree has historically always relied on
         // having ostree -> sysroot/ostree as a symlink in the image to
@@ -119,7 +85,6 @@ impl Storage {
             sysroot,
             run,
             composefs: Default::default(),
-            store,
             imgstore: Default::default(),
         })
     }
@@ -188,27 +153,5 @@ impl Storage {
             .update_timestamps(std::path::Path::new(BOOTC_ROOT))
             .context("update_timestamps")
             .map_err(Into::into)
-    }
-}
-
-impl ContainerImageStore for ostree::Deployment {
-    fn store<'a>(&self) -> Result<Option<Box<dyn ContainerImageStoreImpl>>> {
-        if let Some(origin) = self.origin().as_ref() {
-            if let Some(store) = origin.optional_string("bootc", "backend")? {
-                let store =
-                    crate::spec::Store::from_str(&store, true).map_err(anyhow::Error::msg)?;
-                Ok(Some(load(store)))
-            } else {
-                Ok(None)
-            }
-        } else {
-            Ok(None)
-        }
-    }
-}
-
-pub(crate) fn load(ty: crate::spec::Store) -> Box<dyn ContainerImageStoreImpl> {
-    match ty {
-        crate::spec::Store::OstreeContainer => Box::new(ostree_container::OstreeContainerStore),
     }
 }
